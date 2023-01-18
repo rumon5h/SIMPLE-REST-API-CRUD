ba3f29cf13a3fb2b4fe7500be977661057f56c67
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-18 10:41:33 +0600

    connected mongodb and setup server

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jcvfs6.mongodb.net/?retryWrites=true&w=majority`;
+
+async function run(){
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  client.connect((err) => {
+    const collection = client.db("test").collection("devices");
+    try {
+  
+  
+    } finally {
+      // perform actions on the collection object
+      //   client.close();
+    }
+  });
+  
+}
+
+run().catch(console.dir)
+
+app.get("/", (req, res) => {
+  res.write(`<h2>This is a simple Rest API with CURD operations.</h2>`);
+  res.end();
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on port", PORT);
+});
